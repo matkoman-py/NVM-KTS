3d1c27bacae3cb6904c4798343f282b25074d62f
Petar Markovic <petar.markovic@vega.local>
2021-11-14 18:57:44 +0100

    db fix

--- RestaurantApp/src/main/resources/data.sql
+++ RestaurantApp/src/main/resources/data.sql
@@ -9,12 +9,12 @@ INSERT INTO article (name, description, type, menu_id) VALUES
   
 INSERT INTO users (type, birthday, email, name, surname, employee_type, pincode) VALUES
   ('EMPLOYEE', '1999-8-21 ', 'mateja99@yahoo.com', 'Mateja', 'Cosovic', 'WAITER', 1234),
-  ('EMPLOYEE', '1999-3-05 ', 'suljak99@yahoo.com', 'Marko', 'Suljak', 'KITCHEN_WORKER', 4322),
-  ('EMPLOYEE', '1999-11-25 ', 'cepic@yahoo.com', 'Aleksandar', 'Cepic', 'KITCHEN_WORKER', 5321);
+  ('EMPLOYEE', '1999-3-05 ', 'suljak99@yahoo.com', 'Marko', 'Suljak', 'BARMAN', 4322),
+  ('EMPLOYEE', '1999-11-25 ', 'cepic@yahoo.com', 'Aleksandar', 'Cepic', 'BARMAN', 5321);
   
 INSERT INTO orders (description, order_date, table_number, employee_id) VALUES
-  ('No ketchup', '2021-11-3 12:43:33', 1, 2),
-  ('Extra mayo', '2021-12-3 15:21:00', 3, 2);
+  ('No ketchup', '2021-11-3 12:43:33', 1, 4),
+  ('Extra mayo', '2021-12-3 15:21:00', 3, 4);
 
   
 INSERT INTO ingredient (name, is_allergen) VALUES
