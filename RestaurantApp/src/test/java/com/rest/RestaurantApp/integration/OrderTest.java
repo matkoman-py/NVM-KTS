4f38c32ac8ed2b4c23ece3c9ead8008c73ec861b
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-12-07 21:36:23 +0100

    feature: adds test database

--- RestaurantApp/src/test/java/com/rest/RestaurantApp/integration/OrderTest.java
+++ RestaurantApp/src/test/java/com/rest/RestaurantApp/integration/OrderTest.java
@@ -9,11 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.rest.RestaurantApp.domain.Employee;
+import com.rest.RestaurantApp.dto.EmployeeDTO;
 import com.rest.RestaurantApp.dto.OrderDTO;
+import com.rest.RestaurantApp.services.EmployeeService;
 import com.rest.RestaurantApp.services.OrderService;
 
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
 
 import org.junit.Test;
 
@@ -36,11 +42,25 @@ public class OrderTest {
 		ResponseEntity<OrderDTO[]> responseEntity = restTemplate.getForEntity(
 				"/api/order", OrderDTO[].class);
 
-		
 		OrderDTO[] categories = responseEntity.getBody();
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-		assertEquals(1, categories.length);
+		assertEquals(13, categories.length);
 		
+	}
+	
+	@Test
+	public void testGetOne_ValidId() {
+		ResponseEntity<OrderDTO> responseEntity = restTemplate
+				.getForEntity("/api/order/1", OrderDTO.class);
+
+		OrderDTO order = responseEntity.getBody();
+
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals(order.getArticles().size(), 4);
+		assertEquals(order.getDescription(), "No mustard");
+		assertEquals(order.getEmployeeId(), 3);
+		assertEquals(order.getTableNumber(), 1);
+		assertEquals(order.getOrderDate(), LocalDateTime.of(2021, 1, 3, 12, 43, 33));
 	}
 	
 
